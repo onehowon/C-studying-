c03c4b8dba86cbf629f33a73df49f7ca05c138f3
onehowon <flash0221@naver.com>
2022-06-19 18:48:29 +0900

    2nd commit

---
+++ 명품 C++/My class.cpp
@@ -0,0 +1,8 @@
+class Sample{
+private:
+    // private 멤버 선언. 클래스 내의 멤버 함수만 접근 가능
+public:
+    // public 멤버 선언. 클래스 내외의 모든 함수에게 접근 허용
+protected:
+    // protected 멤버 선언. 클래스 내의 멤버와 상속받은 파생 클래스에만 접근 허용
+};
